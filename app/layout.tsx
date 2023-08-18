9a3965851590e33a03eb9da9bd7cccee2585a5df
dtruant <dtruant@navent.com>
2023-08-18 10:35:26 -0300

    setup tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "memo test",
+  description: "memo test game",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
